0fa9cc3845b23463167c587b40678b5898a7bc86
krasnof.kobi@gmail.com <krasnoff.kobi@gmail.com>
2018-08-04 22:07:07 +0300

    Add project files.

---
+++ UBTechServerExercise/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using UBTechServerExercise.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace UBTechServerExercise
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.Formatters.Add(new BrowserJsonFormatter());
+
+            // New code
+            config.EnableCors();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ UBTechServerExercise/Controllers/StudentsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace UBTechServerExercise.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class StudentsController : ApiController
+    {
+        // GET: api/Students/GetSearchString/{search string}
+        [Route("Api/Students/GetSearchString/{searchString}")]
+        public IEnumerable<tbl_StudentsList> GetSearchString(string searchString)
+        {
+            tbl_StudentsList[] res;
+
+            if (searchString.Length < 2)
+            {
+                return new tbl_StudentsList[0];
+            }
+
+            using (var context = new Database1Entities())
+            {
+                var blog = from el in context.tbl_StudentsList
+                           where el.FirstName.IndexOf(searchString) >= 0 || el.LastName.IndexOf(searchString) >= 0
+                           select el;
+
+                res = blog.ToArray();
+            }
+
+            return res;
+        }
+    }
+}
